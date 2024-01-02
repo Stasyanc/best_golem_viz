c7ee807c189c70ae7a9833d40e62bfe9a979786f
channelstas@gmail.com <channelstas@gmail.com>
2024-01-03 01:02:09 +0400

    better gui

--- demo.py
+++ demo.py
@@ -7,23 +7,22 @@ from utils import draw_graphs_subplots
 from gui import Graph_gui
 import tkinter as tk
 
-def test_viz(size=16,type="gui"):
-
+def test_viz(size=16,type=None):
     node_types = ('a', 'b')
     target_graph = generate_labeled_graph('tree', size, node_labels=node_types)
     pos=nx.spring_layout(target_graph)
     fit = best_viz.vis_quality(target_graph,pos)
     best_pos,best_fit = best_viz.generate_best_viz(target_graph,pos)
-    if type!="gui":
+    if type!=None:
         draw_graphs_subplots(target_graph, target_graph, titles=['Old Graph: '+str(fit), 'Best Graph: '+str(best_fit)],poses=[pos,best_pos])
     else:
         root = tk.Tk()
         app = Graph_gui(root,target_graph,fit,best_fit,poses=[pos,best_pos])
-        root.geometry("1200x700")  # Устанавливаем размер окна
+        app.window()
+        root.geometry("1200x700")
         root.mainloop()
     return target_graph
 
 
 if __name__ == '__main__':
-    print("Введите кол-во вершин")
-    test_viz(16)
+    test_viz()
--- gui.py
+++ gui.py
@@ -1,26 +1,46 @@
 import tkinter as tk
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import networkx as nx
 import matplotlib.pyplot as plt
-from examples.synthetic_graph_evolution.generators import generate_labeled_graph
 from utils import draw_graphs_subplots
+from examples.synthetic_graph_evolution.generators import generate_labeled_graph
+import networkx as nx
+import best_viz
 
 class Graph_gui:
     def __init__(self,master,target_graph,fit,best_fit,poses=[None,None]):
         self.master=master
         self.master.title("Best_Golem_Viz")
-        
-        draw_graphs_subplots(target_graph, target_graph,show=False, titles=['Old Graph: '+str(fit), 'Best Graph: '+str(best_fit)],poses=poses)
-        figure = plt.gcf()
+        self.target_graph=target_graph
+        self.titles=['Old Graph: '+str(fit), 'Best Graph: '+str(best_fit)]
+        self.poses=poses
+        draw_graphs_subplots(self.target_graph,self.target_graph,titles=self.titles,show=False,poses=self.poses)
+        figure = plt.gcf() 
+        self.canvas = FigureCanvasTkAgg(figure)
+        self.canvas.draw()
+        self.textbox=tk.Text(width=5,height=1)
+        self.update_button=tk.Button(self.master,text="Запустить",command=self.update)
+        self.graph_frame = tk.Frame(self.master)
+        self.quit_button = tk.Button(self.master, text="Выход", command=self.master.quit)
+
+    def window(self):
+        self.textbox.pack(side=tk.TOP)
+        self.update_button.pack(side=tk.TOP)
+        self.quit_button.pack(side=tk.TOP)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def update(self):
+        self.canvas.get_tk_widget().destroy()
+        s=self.textbox.get('1.0',tk.END)
+        node_types = ('a', 'b')
+        target_graph = generate_labeled_graph('tree', int(s), node_labels=node_types)
+        pos=nx.spring_layout(target_graph)
+        fit = best_viz.vis_quality(target_graph,pos)
+        best_pos,best_fit = best_viz.generate_best_viz(target_graph,pos)
+        self.target_graph=target_graph
+        self.titles=['Old Graph: '+str(fit), 'Best Graph: '+str(best_fit)]
+        self.poses=[pos,best_pos]
+        draw_graphs_subplots(self.target_graph,self.target_graph,titles=self.titles,show=False,poses=self.poses)
+        figure = plt.gcf() 
         self.canvas = FigureCanvasTkAgg(figure)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        
-        ss=tk.Text(width=5,height=1)
-        self.graph_frame = tk.Frame(self.master)
-        self.graph_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-       # self.contents = tk.StringVar()
-        quit_button = tk.Button(self.master, text="Выход", command=self.master.quit)
-        quit_button.pack(side=tk.BOTTOM)
-      #  ss.pack(side=tk.BOTTOM)
