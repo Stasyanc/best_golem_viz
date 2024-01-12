a8f39abd5ca1d95039324043a3e1cb42602fd534
channelstas@gmail.com <channelstas@gmail.com>
2024-01-12 22:00:42 +0400

    fix

--- gui_demo.py
+++ gui_demo.py
@@ -1,11 +1,4 @@
-import matplotlib.pyplot as plt
-from Intersect import SweepIntersector
-import networkx as nx
-import best_viz
-from examples.synthetic_graph_evolution.generators import generate_labeled_graph
-from utils import draw_graphs_subplots
 from gui import create_window
-import tkinter as tk
 
 def test_viz_gui(size=16):
     tr=create_window(size)
