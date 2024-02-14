cf843b32875083e5ef48a76d6595bd10850301aa
Stasya <channelstas@gmail.com>
2024-02-14 21:33:52 +0400

    fix name file

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ def test_viz(seed,iteration,pop_size,num_gen,size):
     test_table_f.write(str(end_t2-start_t2)+'\t')
     test_table_f.write(str(mv)+'\n')
     plt.close()
-    draw_graphs_subplots(target_graph, target_graph,poses=[pos,best_pos],show=False,path_to_save="test/pic/"+str(j)+".png",titles=["Было:"+str(fit),"Стало:"+str(best_fit)])
+    draw_graphs_subplots(target_graph, target_graph,poses=[pos,best_pos],show=False,path_to_save="test/pic/"+str(size)+"_"+str(j)+".png",titles=["Было:"+str(fit),"Стало:"+str(best_fit)])
     print(str(j)+" граф:\t",file=test_max_v_f)
     print(target_graph._adj,file=test_max_v_f)
     return target_graph
